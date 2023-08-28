9bdb6e111ec4e0ac9b4b78705e123311303fce37
Max Zakharzhevskiy <maxzz@msn.com>
2023-08-27 18:54:03 -0700

    WIP. update build destination

--- @types/websdk.client.d.ts
+++
@@ -1,3 +0,0 @@
-export { WebChannelOptions } from './sdk/channel-definitions';
-export { WebChannelClientImpl } from './sdk/channel-client-implementation';
-export { WebChannelClient } from './sdk/channel-client';
--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
-import meta from "./package.json" assert {type: "json"};
+// @ts-check
 import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 import filesize from "rollup-plugin-filesize";
+import meta from "./package.json" assert {type: "json"};
 
 const extensions = ['.ts', '.js'];
 
@@ -10,67 +11,76 @@ const commonPlugins = [
     nodeResolve({ extensions }),
 ];
 
-const config = {
-    input: "./build/websdk.client.js",
-    output: {
-        file: `dist/${meta.name}.js`,
-        name: "WebSdk",
-        format: "umd",
-        indent: true,
-        extend: true,
-        banner: `//maxzz ${meta.homepage} v${meta.version}`
-    },
-    plugins: [
-        ...commonPlugins,
-        terser(),
-    ],
-};
+function configUmd({ input, output, minify }) {
+    return {
+        input,
+        output: {
+            file: output,
+            name: "WebSdk",
+            format: "umd",
+            indent: true,
+            extend: true,
+            banner: `//maxzz ${meta.homepage} v${meta.version}`
+        },
+        plugins: [
+            ...commonPlugins,
+            minify ? terser() : [],
+        ],
+    };
+}
 
-const configTs = {
-    input: "./build/websdk.client.js",
-    output: {
-        file: `dist_min/${meta.name}.es.js`,
-        name: "WebSdk",
-        format: "es",
-    },
-    plugins: [
-        ...commonPlugins,
-        filesize({ showBeforeSizes: true, showGzippedSize: true }),
-        terser(),
-    ],
-};
+function configTsMin({ input, output }) {
+    return {
+        input,
+        output: {
+            file: output,
+            name: "WebSdk",
+            format: "es",
+        },
+        plugins: [
+            ...commonPlugins,
+            filesize({ showBeforeSizes: "build", showGzippedSize: true }),
+            terser(),
+        ],
+    };
+}
 
-const configTsMin = {
-    input: "./build/websdk.client.js",
-    output: {
-        file: `dist/${meta.name}.es.js`,
-        name: "WebSdk",
-        format: "es",
-    },
-    plugins: [
-        ...commonPlugins,
-    ],
-};
+function configTs({ input, output }) {
+    return {
+        input,
+        output: {
+            file: output,
+            name: "WebSdk",
+            format: "es",
+        },
+        plugins: [
+            ...commonPlugins,
+        ],
+    };
+}
 
-const configTsDefs = {
-    input: "./src/websdk.client.ts",
-    output: {
-        file: `dist_ts/${meta.name}.js`,
-        name: "WebSdk",
-        format: "es",
-    },
-    plugins: [
-        ...commonPlugins,
-        typescript({
-            emitDeclarationOnly: true,
-            declaration: true,
-        })
-    ],
-};
+function configTsDefs({ input, output }) {
+    return {
+        input,
+        output: {
+            file: output,
+            name: "WebSdk",
+            format: "es",
+        },
+        plugins: [
+            ...commonPlugins,
+            typescript({ emitDeclarationOnly: true, declaration: true, })
+        ],
+    };
+}
+
+const scrFromBuild = "./build/websdk.client.js";
+const scrFromTs = "./src/websdk.client.ts";
 
 export default [
-    config,
-    configTs,
-    configTsMin,
-    configTsDefs,
+    configUmd({ input: scrFromBuild, output: `dist/umd/websdk.client.js`, minify: false }),
+    configUmd({ input: scrFromBuild, output: `dist/umd/websdk.client.min.js`, minify: true }),
+    configTsMin({ input: scrFromBuild, output: `dist/index.es.min.js` }),
+    configTs({ input: scrFromBuild, output: `dist/index.js` }),
+    configTsDefs({ input: scrFromTs, output: `dist/index.js` }),
 ];
