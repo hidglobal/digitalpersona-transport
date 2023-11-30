4125a5a917afa79ac068226bc845091c252b5aec
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-29 17:28:39 -0800

    WIP. extract generateSessionKey()

--- src/sdk/channel-client-implementation.ts
+++ src/sdk/channel-client-implementation.ts
@@ -204,43 +204,13 @@ export class WebChannelClientImpl {
     };
 
     private async generateSessionKey(): Promise<ErrorOrDataResult> {
-        try {
-            const srpData = (await configurator.getSessionStorageData())?.srpClient;
-            if (!srpData?.p1 || !srpData.p2 || !srpData.salt) {
-                return { error: "No data available for authentication" };
-            }
-
-            const srpClient = new SRPClient(srpData.p1, srpData.p2);
-
-            let a: BigInteger;
-            do {
-                a = srpClient.srpRandom();
-            } while (!srpClient.canCalculateA(a));
-
-            const A: BigInteger = srpClient.calculateA(a);
-
-            const response = await ajax<{ version: number; B: BigInteger; }>('post', await configurator.getDpHostConnectionUrl(), {
-                username: srpData.p1,
-                A: srpClient.toHexString(A),
-                version: envSdk.version.toString(),
-            });
-
-            envSdk.version = response.version ?? /*old client*/ Math.min(envSdk.version, WebSdkEncryptionSupport.Encryption);
-
-            const B = new BigInteger(response.B, 16);
-            const u = srpClient.calculateU(A, B);
-            const S = srpClient.calculateS(B, srpData.salt, u, a);
-            const K = srpClient.calculateK(S);
-            const M1 = srpClient.calculateM(A, B, K, srpData.salt);
-
-            // we will use SHA256 from K as AES 256bit session key
-            this.sessionKey = cipher.hexToBytes(sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(sjcl.codec.hex.toBits(K))));
-            this.M1 = M1;
-
-            return { data: M1 };
-        } catch (error) {
-            return { error: (error instanceof Error ? error.message : (error as any).toString()) || 'tm.error.key' };
+        const { sessionKey, M1, error } = await generateSessionKey();
+        if (error) {
+            return { error };
         }
+        this.sessionKey = sessionKey || null;
+        this.M1 = M1;
+        return {data: M1};
     }
 
     /**
@@ -293,3 +263,42 @@ export class WebChannelClientImpl {
     }
 
 } //class WebChannelClientImpl
+
+async function generateSessionKey() {
+    try {
+        const srpData = (await configurator.getSessionStorageData())?.srpClient;
+        if (!srpData?.p1 || !srpData.p2 || !srpData.salt) {
+            return { error: "No data available for authentication" };
+        }
+
+        const srpClient = new SRPClient(srpData.p1, srpData.p2);
+
+        let a: BigInteger;
+        do {
+            a = srpClient.srpRandom();
+        } while (!srpClient.canCalculateA(a));
+
+        const A: BigInteger = srpClient.calculateA(a);
+
+        const response = await ajax<{ version: number; B: BigInteger; }>('post', await configurator.getDpHostConnectionUrl(), {
+            username: srpData.p1,
+            A: srpClient.toHexString(A),
+            version: envSdk.version.toString(),
+        });
+
+        envSdk.version = response.version ?? /*old client*/ Math.min(envSdk.version, WebSdkEncryptionSupport.Encryption);
+
+        const B = new BigInteger(response.B, 16);
+        const u = srpClient.calculateU(A, B);
+        const S = srpClient.calculateS(B, srpData.salt, u, a);
+        const K = srpClient.calculateK(S);
+        const M1 = srpClient.calculateM(A, B, K, srpData.salt);
+
+        // we will use SHA256 from K as AES 256bit session key
+        const sessionKey = cipher.hexToBytes(sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(sjcl.codec.hex.toBits(K))));
+
+        return { sessionKey, M1 };
+    } catch (error) {
+        return { error: (error instanceof Error ? error.message : (error as any).toString()) || 'tm.error.key' };
+    }
+}
